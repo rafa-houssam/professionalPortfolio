6f3f1de16b47ff52559b0f13efd7c20fca480184
rafa-houssam <rafahoussam4edu@gmail.com>
2025-07-28 08:39:06 +0100

    cleaning the files

--- app/routes/products/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server"
-import { connectToDB } from "../db"
-
-export async function GET(){
-    const {db}= await connectToDB()
-    const products=await db.collection('products').find({}).toArray()
-    return NextResponse.json({products},{status:200})
-}
