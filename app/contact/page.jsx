caca4092acd5fbeda79693f65887ff93baaa0e9e
rafa-houssam <rafahoussam4edu@gmail.com>
2025-07-29 16:09:53 +0100

    portfolio page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -35,12 +35,12 @@ const ContactPage = () => {
 
   return (
     <motion.div
-      className="h-890"
+      className="h-full"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="h-890 flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex items-center justify-center text-6xl">
           <div className=" ">
@@ -66,7 +66,7 @@ const ContactPage = () => {
         <form
           onSubmit={sendEmail}
           ref={form}
-          className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24 "
+          className="h-1/2 lg:h-full lg:w-1/2 bg-red-50 rounded-xl text-xl flex flex-col gap-8 justify-center p-24 font-light"
         >
           <span>Dear Rafa Houssam,</span>
           <textarea
