ca31ee259ab51906edc926059c99fedf7a7c3747
rafa-houssam <rafahoussam4edu@gmail.com>
2025-07-29 15:04:27 +0100

    added signature

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -43,7 +43,7 @@ const AboutPage = () => {
             <p className="text-lg font-light">
               I'm a dedicated and ambitious Computer Science student at ESI Algiers, passionate about building innovative solutions through code. My journey in technology is driven by curiosity and a desire to make meaningful impact.
 
-I consider myself a determined learner, always working hard to build strong, real-world skills in web development and AI. I'm actively involved in projects, clubs, and continuous learning to make the most of my journey.
+              I consider myself a determined learner, always working hard to build strong, real-world skills in web development and AI. I'm actively involved in projects, clubs, and continuous learning to make the most of my journey.
             </p>
             {/* BIOGRAPHY QUOTE */}
             <span className="italic font-light">
@@ -51,19 +51,7 @@ I consider myself a determined learner, always working hard to build strong, rea
             </span>
             {/* BIOGRAPHY SIGN SVG*/}
             <div className="self-end">
-              <svg
-                width="185"
-                height="77"
-                viewBox="0 0 370 114"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M66 2C66 29.4851 68.6687 64.5118 49.3333 87.4444C42.4997 95.5495 35.7683 97.6796 26.2222 101C20.002 103.164 8.87322 103.873 4 99C-0.260934 94.7391 2.94804 88.1756 8.22222 86.2222C13.7053 84.1915 17.942 84 23.7778 84C33.359 84 41.3193 83.5602 50.2222 87C56.6125 89.469 63.5773 91.9131 69.5555 95.5C75.4778 99.0533 87.1838 104.357 93.5 99.4444C96.1292 97.3995 96.2752 92.5118 96.9444 89.5C97.9646 84.9092 92.6432 83.2024 89 83C84.472 82.7484 82.3397 81.8856 82 88C81.8025 91.5554 83.5627 94.4193 86 97C88.9648 100.139 92.0717 100.96 96 98.7778C99.3106 96.9386 98 90.7299 98 87.5C98 85.0327 98.4365 83.1348 99.2222 80.7778C100.357 77.3743 99.2311 78.4486 101.5 77.9444C105.352 77.0886 108 76.4766 108 81.5C108 85.6646 109 89.3473 109 93.5C109 100.142 108.863 95.0454 110.5 91.4444C112.765 86.4616 116.631 81.205 121.5 78.5C127.057 75.4129 126 82.1509 126 85.5C126 92.5532 124.42 102 134 102C142.932 102 153 102.569 153 91.2222C153 87.1735 153.772 81.3206 148 81C141.934 80.663 142.107 81.8068 139.5 86.5C134.378 95.7204 137.972 105 149.5 105C153.589 105 153.996 99.8977 155.5 96.8889C157.902 92.0843 161 85.4067 161 80C161 74.0547 158.407 82.7413 157.222 84.2222C155.194 86.7574 155 92.5718 155 95.7778C155 99.9302 153.8 104.999 158 107.222C161.954 109.316 164.884 106.382 167.778 103.778C171.15 100.743 175.896 99.1107 180 97C186.143 93.8409 191.659 91.4099 198.222 89.2222C206.505 86.4614 214.839 87 223.5 87C230.613 87 231.628 104 222.5 104C216.954 104 199.251 107.814 207 95.2222C211.456 87.9805 214.484 80.6007 220 73.7778C229.781 61.6805 242.696 50.8197 256.222 43C264.769 38.0591 274.192 34.6264 283 30.2222C286.55 28.4473 280.07 32.3343 278.5 33.5556C271.707 38.8391 266.609 45.3914 260.556 51.4444C255.356 56.6444 250.682 61.459 246.5 67.5C242.917 72.6757 239.364 77.3825 236.556 83C233.829 88.4524 231.82 94.3142 228.556 99.4444C226.693 102.371 225.518 107.823 222.5 109.5C214.795 113.78 217.517 100.438 218.056 95.0556C218.678 88.8318 227.982 85.7572 233.056 88.6111C239.614 92.3003 245.506 97.7883 252 101.778C254.886 103.551 259.46 107 263 107C271.267 107 273.32 81.9392 268.778 77.2222C264.112 72.3774 261.206 80.5039 261 84C260.576 91.2135 257.836 96.9269 264.778 102C272.242 107.454 285.041 112.276 292.111 104.833C298.002 98.6323 304.301 90.8902 308.556 83.4444C310.355 80.295 310.132 84.6251 309.444 86C305.387 94.1158 303 102.264 303 111.5C303 116.021 337.534 99.1863 340.5 98C347.33 95.2679 355.47 93.8299 361.778 90C363.935 88.6902 365.473 88 368 88"
-                  stroke="black"
-                  strokeWidth="2"
-                />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="100" height="100" viewBox="1.9700000286102295 5.349999904632568 102.5999984741211 35.40999984741211"><path d="M35.37 36.07L35.37 36.07Q34.82 37.25 33.87 37.25L33.87 37.25Q33.14 37.25 30.31 36.52L30.31 36.52L22.73 34.67Q19.28 33.93 16.13 33.16L16.13 33.16Q14.63 32.93 13.11 32.71L13.11 32.71Q10.33 32.21 9.41 31.56L9.41 31.56Q7.13 29.92 7.13 29.20L7.13 29.20Q7.13 27.71 8.69 27.01L8.69 27.01Q8.77 26.99 12.13 26.15L12.13 26.15Q15.27 25.41 16.41 24.75L16.41 24.75Q18.91 23.30 22.46 19.59L22.46 19.59Q26.33 15.55 26.33 13.03L26.33 13.03Q26.33 9.30 23.46 8.22L23.46 8.22Q21.89 7.64 17.44 7.64L17.44 7.64Q15.64 7.64 14.24 7.99L14.24 7.99Q13.40 8.24 11.66 8.73L11.66 8.73Q9.84 9.20 6.68 11.62L6.68 11.62Q6.04 12.50 4.63 14.14L4.63 14.14Q3.98 13.63 3.98 12.89L3.98 12.89Q3.98 10.78 7.25 8.98L7.25 8.98Q9.14 7.93 12.44 6.78L12.44 6.78Q16.58 5.35 18.95 5.35L18.95 5.35Q23.71 5.35 26.02 6.99L26.02 6.99Q26.58 7.38 27.71 9.54Q28.85 11.70 28.85 12.38L28.85 12.38Q28.85 15.80 25 20.08L25 20.08Q22.07 23.32 18.22 25.90L18.22 25.90Q17.73 26.25 15.47 27.52L15.47 27.52Q14.30 27.75 13.14 28.01L13.14 28.01Q10.84 28.48 10.37 28.87L10.37 28.87Q10.59 29.43 13.16 29.96L13.16 29.96Q15 30.31 16.82 30.68L16.82 30.68Q17.83 30.92 20.45 31.58L20.45 31.58Q24.00 32.52 35.37 36.07ZM8.24 13.18L8.24 14.32Q8.24 14.65 7.38 17.52L7.38 17.52Q6.43 20.70 6.31 21.35L6.31 21.35Q5.14 28.03 4.92 36.11L4.92 36.11Q4.69 37.29 4.41 39.67L4.41 39.67Q4.34 39.73 2.54 40.76L2.54 40.76Q1.97 36.72 1.97 35.49L1.97 35.49Q1.97 35.04 2.71 30.31L2.71 30.31Q3.73 23.75 4.55 20.02L4.55 20.02Q4.98 18.07 6.05 14.59L6.05 14.59Q6.54 14.04 7.03 12.95L7.03 12.95L8.24 13.18ZM54.47 39.06L54.47 39.94Q54.06 40.51 53.57 40.51L53.57 40.51Q53.26 40.51 51.46 38.63L51.46 38.63Q49.67 36.74 49.06 36.74L49.06 36.74Q49.06 36.74 49.06 36.74L49.06 36.74Q49.06 36.78 44.80 38.38L44.80 38.38Q40.45 40 39.26 40L39.26 40Q38.28 40 37.09 39.04Q35.90 38.09 35.90 37.25L35.90 37.25Q35.90 35.08 40.64 32.19L40.64 32.19Q45.10 29.45 47.44 29.45L47.44 29.45Q48.42 29.45 49.20 30.57L49.20 30.57L49.20 31.41Q48.81 31.97 48.30 31.97L48.30 31.97Q48.20 31.97 47.63 31.72Q47.05 31.46 46.93 31.46L46.93 31.46Q46.15 31.46 43.36 33.07L43.36 33.07Q40.70 34.57 39.80 35.31L39.80 35.31Q38.91 36.09 38.65 37.09L38.65 37.09Q39.34 37.73 40.27 37.73L40.27 37.73Q40.76 37.73 44.98 36.11L44.98 36.11Q49.18 34.47 50.06 34.47L50.06 34.47Q51.35 34.47 51.91 35.12L51.91 35.12Q52.05 35.27 52.91 36.88L52.91 36.88Q53.55 38.09 54.47 39.06L54.47 39.06ZM90.47 12.99L90.47 12.99Q90.47 12.97 90.47 12.97L90.47 12.97Q90.43 12.97 90.16 14.00L90.16 14.00Q86.74 9.24 80.33 9.24L80.33 9.24Q78.18 9.24 75.74 10.27L75.74 10.27Q73.13 11.35 71.95 12.95L71.95 12.95Q70.63 14.77 69.32 18.44L69.32 18.44Q67.91 22.29 67.91 24.28L67.91 24.28Q67.91 24.90 68.16 26.86L68.16 26.86Q68.42 28.98 68.46 29.38L68.46 29.38Q69.32 30.06 69.77 30.06L69.77 30.06Q69.96 30.06 70.70 29.86L70.70 29.86Q71.46 29.67 71.50 29.67L71.50 29.67Q73.01 29.51 78.11 29.51L78.11 29.51Q81.45 29.51 84.45 29.71L84.45 29.71Q84.28 29.71 83.61 30.23L83.61 30.23L83.67 30.78L76.43 31.21Q72.01 31.46 69.63 33.01L69.63 33.01Q70.21 35.08 72.15 38.46L72.15 38.46L72.15 39.53Q71.95 39.88 71.41 39.88L71.41 39.88Q70.88 39.88 68.71 36.35L68.71 36.35Q66.50 32.83 66.02 32.83L66.02 32.83Q64.88 32.83 62.77 33.26L62.77 33.26Q62.13 33.42 60.20 34.12L60.20 34.12Q58.28 34.84 57.97 34.84L57.97 34.84Q56.89 34.84 55.61 33.52L55.61 33.52Q55.61 33.48 55.61 33.44L55.61 33.44Q55.61 32.56 56.43 32.19L56.43 32.19Q58.69 31.13 65.45 30.51L65.45 30.51Q65.84 30.23 66.04 29.92L66.04 29.92Q65.76 27.62 65.76 25.41L65.76 25.41Q65.76 17.79 69.10 12.77L69.10 12.77Q72.91 6.99 80.18 6.99L80.18 6.99Q83.48 6.99 86.58 8.40L86.58 8.40Q90.47 10.18 90.47 12.99ZM104.57 39.06L104.57 39.94Q104.16 40.51 103.67 40.51L103.67 40.51Q103.36 40.51 101.56 38.63L101.56 38.63Q99.77 36.74 99.16 36.74L99.16 36.74Q99.16 36.74 99.16 36.74L99.16 36.74Q99.16 36.78 94.90 38.38L94.90 38.38Q90.55 40 89.36 40L89.36 40Q88.38 40 87.19 39.04Q86.00 38.09 86.00 37.25L86.00 37.25Q86.00 35.08 90.74 32.19L90.74 32.19Q95.20 29.45 97.54 29.45L97.54 29.45Q98.52 29.45 99.30 30.57L99.30 30.57L99.30 31.41Q98.91 31.97 98.40 31.97L98.40 31.97Q98.30 31.97 97.72 31.72Q97.15 31.46 97.03 31.46L97.03 31.46Q96.25 31.46 93.46 33.07L93.46 33.07Q90.80 34.57 89.90 35.31L89.90 35.31Q89.00 36.09 88.75 37.09L88.75 37.09Q89.43 37.73 90.37 37.73L90.37 37.73Q90.86 37.73 95.08 36.11L95.08 36.11Q99.28 34.47 100.16 34.47L100.16 34.47Q101.45 34.47 102.01 35.12L102.01 35.12Q102.15 35.27 103.01 36.88L103.01 36.88Q103.65 38.09 104.57 39.06L104.57 39.06Z" fill="black" /></svg>
             </div>
             {/* BIOGRAPHY SCROLL SVG */}
             <motion.svg
@@ -134,10 +122,10 @@ I consider myself a determined learner, always working hard to build strong, rea
                 Numpy
               </div>
               <div className="rounded font-light  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-               Python
+                Python
               </div>
               <div className="rounded font-light  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-               Django
+                Django
               </div>
               <div className="rounded font-light  p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 Postman
@@ -244,16 +232,16 @@ I consider myself a determined learner, always working hard to build strong, rea
                     2024 - Present
                   </div>
                   {/* JOB COMPANY */}
-                 <div className="flex flex-row gap-2">
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    next.js
-                  </div>
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    html/css/js
-                  </div>
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                   Tailwind
-                  </div></div>
+                  <div className="flex flex-row gap-2">
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                      next.js
+                    </div>
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                      html/css/js
+                    </div>
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                      Tailwind
+                    </div></div>
                 </div>
                 {/* CENTER */}
                 <div className="w-1/6 flex justify-center">
@@ -282,7 +270,7 @@ I consider myself a determined learner, always working hard to build strong, rea
                 <div className="w-1/3 ">
                   {/* JOB TITLE */}
                   <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                   backend developer
+                    backend developer
                   </div>
                   {/* JOB DESC */}
                   <div className="p-3 text-sm italic font-light">
@@ -294,12 +282,12 @@ I consider myself a determined learner, always working hard to build strong, rea
                   </div>
                   {/* JOB COMPANY */}
                   <div className="flex flex-row gap-2">
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    python
-                  </div>
-                  <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
-                    Django
-                  </div></div>
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                      python
+                    </div>
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                      Django
+                    </div></div>
                 </div>
               </div>
               {/* EXPERIENCE LIST ITEM */}
@@ -308,17 +296,28 @@ I consider myself a determined learner, always working hard to build strong, rea
                 <div className="w-1/3 ">
                   {/* JOB TITLE */}
                   <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
-                    Freelancer{" "}
+                    Data science and AI {" "}
                   </div>
                   {/* JOB DESC */}
                   <div className="p-3 text-sm italic font-light">
-                    I provided web solutions, applying a range of technologies
-                    to address client requirements.{" "}
+                    "I began exploring Data Science and AI by applying a variety of tools to solve problems, gradually deepening my understanding through hands-on projects and continuous learning{" "}
                   </div>
                   {/* JOB DATE */}
                   <div className="p-3 text-red-400 text-sm font-semibold">
                     since 2025{" "}
                   </div>
+                   <div className="flex flex-row gap-2">
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                      python
+                    </div>
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                      Numpy
+                    </div>
+                    <div className="p-1 rounded bg-white text-sm font-semibold w-fit">
+                      Pandas
+                    </div>
+                    </div>
+                  
                 </div>
                 {/* CENTER */}
                 <div className="w-1/6 flex justify-center">
@@ -329,20 +328,20 @@ I consider myself a determined learner, always working hard to build strong, rea
                   </div>
                 </div>
                 {/* RIGHT */}
-                 <div className="w-1/3 "></div>
-                
+                <div className="w-1/3 "></div>
+
               </div>
             </motion.div>
           </div>
         </div>
         {/* SVG CONTAINER */}
-       <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2">
+        <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2">
           <Brain scrollYProgress={scrollYProgress} />
         </div>
 
 
       </div>
-      
+
     </motion.div>
   );
 };
