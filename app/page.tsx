0156f6f25f5e09617d7039130643e65ea4b08d11
rafa-houssam <rafahoussam4edu@gmail.com>
2025-11-07 15:01:25 +0100

    last one

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,7 @@ const Homepage = () => {
 
   return (
     <motion.div
-      className="h-screen overflow-hidden"
+      className="mt-17"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
